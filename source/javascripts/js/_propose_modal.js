1215e96add2b83ac83112b1fe937b7e685faaab6
germainloret <germain.loret@gmail.com>
2017-10-21 12:39:14 +0200

    form ajaxified :)

--- source/javascripts/js/_propose_modal.js
+++ source/javascripts/js/_propose_modal.js
@@ -1,3 +1,6 @@
+// set redirection
+document.getElementsByName('_next')[0].value = window.location.href
+
 // set comportement for modal
 const modalButtons = document.querySelectorAll("[data-target='propose-modal']");
 const closeButton = document.querySelector('#close-propose-modal');
@@ -20,6 +23,37 @@ const form = document.querySelector('#propose-form');
 
 sendWithAjax = (e) => {
   e.preventDefault();
+
+  const msgLoad = '<div class="alert alert-loading">Message en cours d\'envoi</div>';
+  const msgSuccess = '<div class="alert alert-success">Message reçu frère !</div>';
+  const msgError = '<div class="alert alert-error">Oups, une erreur est survenue :(</div>';
+
+  const updateMsg = (nodes, msg) => {
+    const messageStatus = document.querySelectorAll('.alert-loading');
+    for (i = 0; i < messageStatus.length; i++) {
+      statusMessages[i].style.display = 'none';
+    }
+    nodes.insertAdjacentHTML('beforeend', msg)
+  }
+
+  fetch('//formspree.io/germain.loret@gmail.com', {
+      method: 'post',
+      mode: 'cors',
+      headers: {
+        'Accept': 'application/json',
+      },
+      body: new FormData(form)
+    }).then(function(response) {
+      console.log(response)
+      if (!response.ok) {
+        updateMsg(form, msgError);
+        return Promise.reject(new Error(response.statusText))
+      }
+      updateMsg(form, msgSuccess);
+      return Promise.resolve(response)
+    }).catch(function(error) {
+      updateMsg(form, msgError);
+    });
 }
 
 form.addEventListener('submit', sendWithAjax)
